9d152873efdf86a9bb2f8c1921b4ab34c962f880
Vo Phu Thanh <bovo2004@gmail.com>
2026-06-25 17:37:33 +0700

    feat: init user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { HealthController } from './health/health.controller';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, UsersModule],
   controllers: [AppController, HealthController],
   providers: [AppService],
 })
